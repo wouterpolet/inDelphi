87ee97aa3435dfa9f7200b0f7b807e7663c19448
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2022-03-13 18:14:53 +0100

    Update 3f-plot.py

--- src-modeling-and-analysis/3f-plot.py
+++ src-modeling-and-analysis/3f-plot.py
@@ -1,21 +1,25 @@
 # Figure 3f plotting
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-path = "G:/My Drive/3) Work/Study/TU_Delft_MSc_Nanobiology/3-courses/Machine-Learning-in-Bioinformatics/inDelphi/shared/inDelphi/out/aaa/statistics/prediction_output.csv"
-predictions = pd.read_csv(path)
+franz_path = "G:/My Drive/3) Work/Study/TU_Delft_MSc_Nanobiology/3-courses/Machine-Learning-in-Bioinformatics/inDelphi/shared/inDelphi/out/aaa/statistics/prediction_output.csv"
+predictions = pd.read_csv(franz_path)
 
-fig_3f_data_del = predictions['Highest Del Rate'].apply(lambda x: x*100)
-fig_3f_data_ins = predictions['Highest Ins Rate'].apply(lambda x: x*100)
+predictions['Highest Del Rate'] = predictions['Highest Del Rate'].apply(lambda x: x*100)
+predictions['Highest Ins Rate'] = predictions['Highest Ins Rate'].apply(lambda x: x*100)
 #num_bins = 100
+bins_range = np.asarray(range(1,101))
+print(bins_range)
 
-predictions.hist(column=['Highest Del Rate', 'Highest Ins Rate'])
+fig1 = plt.subplot()
+fig_3f_data_del = np.asarray(predictions['Highest Del Rate'])
+plt.hist(fig_3f_data_del, range=(0, 100), bins=bins_range)
 plt.show()
 
-#plt.hist(fig_3f_data_del, num_bins)
-# plt.hist(fig_3f_data_del)
-# plt.show()
-# print(max(fig_3f_data_del))
-# print(min(fig_3f_data_del))
+fig2 = plt.subplot()
+fig_3f_data_ins = np.asarray(predictions['Highest Ins Rate'])
+plt.hist(fig_3f_data_ins, range=(0, 100), bins=bins_range)
+plt.show()
 
 
