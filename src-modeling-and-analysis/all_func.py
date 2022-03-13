12da606a0dbce40f516f80b8d6460ecdc2ab8395
Jonathan Borg <jonathanborg1996@gmail.com>
2022-03-13 16:22:50 +0100

    added missing functions for predict all

--- src-modeling-and-analysis/all_func.py
+++ src-modeling-and-analysis/all_func.py
@@ -613,6 +613,69 @@ def get_indel_len_pred(pred_all_df):
   return indel_len_pred, fs                     # return dict: {+1 = [..], -1 = [..], ..., -60 = [..]} and fs = {'+0': [..], '+1': [..], '+2': [..]}
 
 
+def get_gc_frac(seq):
+  return (seq.count('C') + seq.count('G')) / len(seq)
+
+
+# for a given pair of resected left and right strands, equally long
+def find_microhomologies(left, right):
+  # TAGATT - TATAGG = 0
+  start_idx = max(len(right) - len(left), 0)
+  mhs = []
+  mh = [start_idx]
+  # for each base in the overhangs
+  for idx in range(min(len(right), len(left))):
+    # {--left[idx] == right[idx]--} = {--left[idx] complementary to reverse_right[star_idx+idx]--}
+    if left[idx] == right[start_idx + idx]:
+      #           TAGATT    2 MHs
+      #           || |
+      #           ATATCC
+      # gt pos    123456
+      # MH 1 del outcome: GTGCTCTTAACTTTCACTTTATATAGGGTTAATAAATGGGAATTTATAT
+      # MH 2 del outcome: GTGCTCTTAACTTTCACTTTATAGAGGGTTAATAAATGGGAATTTATAT
+      mh.append(start_idx + idx + 1)
+    else:
+      mhs.append(mh)
+      mh = [start_idx + idx + 1]
+  mhs.append(mh)
+  return mhs
+
+#                              0123456789012345678901234567890123456789012345678901234
+# MH 1 outcome:                GTGCTCTTAACTTTCACTTTATA------TAGGGTTAATAAATGGGAATTTATAT, gt pos 2, del len 6
+# MH 2 outcome:                GTGCTCTTAACTTTCACTTTATAGA------GGGTTAATAAATGGGAATTTATAT, gt pos 4, del len 6
+# cutsite 27:                  GTGCTCTTAACTTTCACTTTATAGATT
+#                                                         TATAGGGTTAATAAATGGGAATTTATAT (this is not reverse strand)
+# print 'Using DELLEN_LIMIT = %s' % (DELLEN_LIMIT) TATCTAAATATCCCAATTATTTACCCTTAAATATA
+# TODO shouldnt deletion limit be less or equal to the sequence len? how can it be larger?
+def featurize_seq(seq, cutsite, del_len_limit=60):  # for each gRNA sequence, e.g. GTGCTCTTAACTTTCACTTTATAGATTTATAGGGTTAATAAATGGGAATTTATAT
+  mh_lens, gc_fracs, gt_poss, del_lens = [], [], [], []
+  for del_len in range(1, del_len_limit):
+    left = seq[cutsite - del_len: cutsite]  # get 3' overhang nucleotides on the left            TAGATT
+    right = seq[cutsite: cutsite + del_len]  # and 5' overhang on the right of cutsite                  TATAGG (used to model the 3' overhang)
+    # complementary 3' overhang:     ATATCC
+
+    # e.g. del lengh = 6, mhs = [[0, 1, 2], [3, 4], [5], [6]]
+    mhs = find_microhomologies(left, right)
+    for mh in mhs:          # len      3        2      1     1
+      mh_len = len(mh) - 1  # len-1    2        1      0     0
+      if mh_len > 0:  # i.e. if true MH
+        gtpos = max(mh)  # for MH1, genotype position = 2, for MH2, genotype position = 4
+        gt_poss.append(gtpos)
+
+        s = cutsite - del_len + gtpos - mh_len  # 27 - 6 + 2 - 2 = 21, cutsite is b/w 27 and 28 (python 26 and 27), cutsite labelled at 27 on python
+        e = s + mh_len  # 21 + 2 = 23
+        mh_seq = seq[s: e]  # seq[21:23] = TA
+        gc_frac = get_gc_frac(mh_seq)
+
+        mh_lens.append(mh_len)  # 2
+        gc_fracs.append(gc_frac)  # 0%
+        del_lens.append(del_len)  # 6
+
+  # all MHs for each resection length, their gc fractions, deltas and deletion lengths
+  #      90x1     90x1      90x1     90x1 lists
+  return mh_lens, gc_fracs, gt_poss, del_lens
+
+
 # TODO fix / optimize
 def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
   # Predict 1 bp insertions and all deletions (MH and MH-less)
@@ -626,7 +689,7 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
   #####
   # Predict MH deletions
 
-  mh_len, gc_frac, gt_pos, del_len = featurize(seq, cutsite)
+  mh_len, gc_frac, gt_pos, del_len = featurize_seq(seq, cutsite)
   # for this sequence context and cutsite: return all MHs for each resection length, their gc fractions, deltas and deletion lengths
 
   # Form inputs
@@ -637,8 +700,8 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
   mh_scores = nn_match_score_function(nn_params, pred_input)  # nn_params are the trained MH-NN params
   mh_scores = mh_scores.reshape(mh_scores.shape[0], 1)
   Js = del_lens.reshape(del_lens.shape[0], 1)
-  unfq = np.exp(
-    mh_scores - 0.25 * Js)  # unnormalised MH-NN phi for each MH (each of which corresponds to a unique genotype)
+  # unnormalised MH-NN phi for each MH (each of which corresponds to a unique genotype)
+  unfq = np.exp(mh_scores - 0.25 * Js)
 
   # Add MH-less contribution at full MH deletion lengths
   mh_vector = np.array(mh_len)
@@ -655,14 +718,14 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
 
   # Store predictions to combine with mh-less deletion predictions
   pred_del_len = copy.copy(del_len)  # prediction deletion lenghts
-  pred_gt_pos = copy.copy(gt_pos)  # prediction deltas             these 2 together correspond to a unique genotype
+  pred_gt_pos = copy.copy(gt_pos)  # prediction deltas these 2 together correspond to a unique genotype
 
   ################################################################
   #####
   ##### Predict MH and MH-less deletions
   #####
   # Predict MH-less deletions
-  mh_len, gc_frac, gt_pos, del_len = featurize(seq, cutsite)  # same results as previously
+  mh_len, gc_frac, gt_pos, del_len = featurize_seq(seq, cutsite)  # same results as previously
 
   unfq = list(unfq)  # unnormalised MH deletion genotype freq distribution
 
@@ -681,12 +744,11 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
 
   mh_vector = np.array(mh_len)
   for dl in nonfull_dls:  # for each deletion length 1- 60 unaccounted for by MH-NN predictions
-    mhless_score = nn_match_score_function(nn2_params,
-                                                 np.array(dl))  # nn2_params are the trained MH-less NN parameters
+    mhless_score = nn_match_score_function(nn2_params, np.array(dl))  # nn2_params are the trained MH-less NN parameters
     mhless_score = np.exp(mhless_score - 0.25 * dl)  # get its the MH-less phi
 
-    unfq.append(mhless_score)  # unnormalised scores for MH-based deletion genotypes
-    #     + unnormalised scores for each unacccounted for MH-less based genotype
+    # unnormalised scores for MH-based deletion genotypes + unnormalised scores for each unacccounted for MH-less based genotype
+    unfq.append(mhless_score)
     pred_gt_pos.append('e')  # gtpos = delta, but delta position = e?
     pred_del_len.append(dl)  # deletion length
 
@@ -713,8 +775,7 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
   dlpred = []
   for dl in range(1, 28 + 1):  # for each deletion length 1:28
     crit = (pred_del_df['Length'] == dl)  # select the predicted dels with that del length
-    dlpred.append(
-      sum(pred_del_df[crit]['Predicted_Frequency']))  # store the predicted freq of all dels with that length
+    dlpred.append(sum(pred_del_df[crit]['Predicted_Frequency']))  # store the predicted freq of all dels with that length
   dlpred = np.array(dlpred) / sum(dlpred)  # normalised frequency distribution of deletion lengths
   norm_entropy = entropy(dlpred) / np.log(len(dlpred))  # precision score of ^ <- input to k-nn
 
@@ -734,13 +795,11 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
   #   the fraction frequency of 1bp ins over all ins and dels
   # Predict 1 bp genotype frequencies
   pred_1bpins_d = defaultdict(list)  # empty dict
-  for ins_base in bp_model[
-    fivebase]:  # structure of bp_model in e5 line 107     e.g. -4 base = T, bp_model[T] retuns e5 line 112
+  # structure of bp_model in e5 line 107     e.g. -4 base = T, bp_model[T] retuns e5 line 112
+  for ins_base in bp_model[fivebase]:
     # for each base {A,C,G,T,} when -4 base is T:
     freq = bp_model[fivebase][ins_base]  # e.g. freq = avg. freq of A when -4 base is T
-    freq *= rate_1bpins / (
-          1 - rate_1bpins)  # e.g. freq of ins_base A =  ratio between fraction frequency of A as 1bp ins when -4 base is T
-    #                                          and the fraction frequency of all deletions
+    freq *= rate_1bpins / (1 - rate_1bpins)  # e.g. freq of ins_base A =  ratio between fraction frequency of A as 1bp ins when -4 base is T and the fraction frequency of all deletions
     # the division by denominator is required to normalise properly at the last line before return
     pred_1bpins_d['Category'].append('ins')
     pred_1bpins_d['Length'].append(1)
@@ -758,14 +817,6 @@ def predict_all(seq, cutsite, rate_model, bp_model, normalizer):
 def bulk_predict(header, sequence, dd, dd_shuffled, df_out_dir):
   # Input: A specific sequence
   # Find all Cas9 cutsites, gather metadata, and run inDelphi
-  try:
-    # header is of FASTA type from NCBI
-    ans = parse_header(header)
-    # gene id in database / chromosome number / start of seq - 30 / end of sequence + 30
-    gene_kgid, chrom, start, end = ans
-  except:
-    return
-
   for idx in range(len(sequence)):  # for each base in the sequence
     # this loop finishes only each of 5% of all found cutsites with 60-bp long sequences containing only ACGT
     seq = ''
@@ -811,18 +862,6 @@ def bulk_predict(header, sequence, dd, dd_shuffled, df_out_dir):
       #
       local_cutsite = 30
       grna = seq_context[13:33]
-      cutsite_coord = start + idx
-      unique_id = '%s_%s_hg38_%s_%s_%s' % (gene_kgid, grna, chrom, cutsite_coord, orientation)
-
-      # the SpCas9 gRNAs targeting exons and introns
-      d['Sequence Context'].append(seq_context)
-      d['Local Cutsite'].append(local_cutsite)
-      d['Chromosome'].append(chrom)
-      d['Cutsite Location'].append(cutsite_coord)
-      d['Orientation'].append(orientation)
-      d['Cas9 gRNA'].append(grna)
-      d['Gene kgID'].append(gene_kgid)
-      d['Unique ID'].append(unique_id)
 
       # Make predictions for each SpCas9 gRNA targeting exons and introns
       ans = predict_all(seq_context, local_cutsite,  # seq_context is a tuple/pair? of seq and shuffled_seq
@@ -948,6 +987,7 @@ def predict_all_items(all_data, df_out_dir, nn_params, nn2_params, rate_model, b
   # for i, line in enumerate(all_data):
   for i, line in all_data.iterrows():
     header = line['name']
+    grna = line['grna']
     sequence = line['target']
 
     # TODO check what to do
@@ -956,8 +996,65 @@ def predict_all_items(all_data, df_out_dir, nn_params, nn2_params, rate_model, b
     #   continue
 
     # predict for a single exon/intron
-    bulk_predict(header, sequence, dd, dd_shuffled, df_out_dir)
-    dd, dd_shuffled, num_flushed = maybe_flush(dd, dd_shuffled, data_nm, split, num_flushed)
+    # bulk_predict(header, sequence, dd, dd_shuffled, df_out_dir)
+    # dd, dd_shuffled, num_flushed = maybe_flush(dd, dd_shuffled, data_nm, split, num_flushed)
+
+    # local_cutsite = find where grna starts, add the total len of the grna and subtract 3 (PAM)
+    local_cutsite = sequence.index(grna) + len(grna) - 3
+    # seq_context is a tuple/pair? of seq and shuffled_seq
+    # trained k-nn, bp summary dict, normalizer
+    ans = predict_all(sequence, local_cutsite, rate_model, bp_model, normalizer)
+    pred_del_df, pred_all_df, total_phi_score, ins_del_ratio = ans
+
+    # normalised frequency distributon on indel lengths
+    indel_len_pred, fs = get_indel_len_pred(pred_all_df)
+
+    #
+    # Store prediction statistics
+    #
+    d['Total Phi Score'].append(total_phi_score)
+    d['1ins/del Ratio'].append(ins_del_ratio)
+
+    d['1ins Rate Model'].append(rate_model)
+    d['1ins bp Model'].append(bp_model)
+    d['1ins normalizer'].append(normalizer)
+
+    d['Frameshift +0'].append(fs['+0'])
+    d['Frameshift +1'].append(fs['+1'])
+    d['Frameshift +2'].append(fs['+2'])
+    d['Frameshift'].append(fs['+1'] + fs['+2'])
+
+    crit = (pred_del_df['Genotype Position'] != 'e')
+    s = pred_del_df[crit]['Predicted_Frequency']
+    s = np.array(s) / sum(s)
+    del_gt_precision = 1 - entropy(s) / np.log(len(s))
+    d['Precision - Del Genotype'].append(del_gt_precision)
+
+    dls = []
+    for del_len in range(1, 60):
+      dlkey = -1 * del_len
+      dls.append(indel_len_pred[dlkey])
+    dls = np.array(dls) / sum(dls)
+    del_len_precision = 1 - entropy(dls) / np.log(len(dls))
+    d['Precision - Del Length'].append(del_len_precision)
+
+    crit = (pred_all_df['Genotype Position'] != 'e')
+    s = pred_all_df[crit]['Predicted_Frequency']
+    s = np.array(s) / sum(s)
+    all_gt_precision = 1 - entropy(s) / np.log(len(s))
+    d['Precision - All Genotype'].append(all_gt_precision)
+
+    negthree_nt = seq_context[local_cutsite - 1]
+    negfour_nt = seq_context[local_cutsite]
+    d['-4 nt'].append(negfour_nt)
+    d['-3 nt'].append(negthree_nt)
+
+    crit = (pred_all_df['Category'] == 'ins')
+    highest_ins_rate = max(pred_all_df[crit]['Predicted_Frequency'])
+    crit = (pred_all_df['Category'] == 'del') & (pred_all_df['Genotype Position'] != 'e')
+    highest_del_rate = max(pred_all_df[crit]['Predicted_Frequency'])
+    d['Highest Ins Rate'].append(highest_ins_rate)
+    d['Highest Del Rate'].append(highest_del_rate)
 
     if (i - 1) % 50 == 0 and i > 1:
       print('%s pct, %s' % (i / 500, datetime.datetime.now()))
