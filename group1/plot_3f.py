939ae3cc14b6ae8c2707e8624b66a68d2ff14e08
Jonathan Borg <jonathanborg1996@gmail.com>
2022-03-14 16:53:09 +0100

    Added violin plot (WIP)

--- group1/plot_3f.py
+++ group1/plot_3f.py
@@ -1,8 +1,8 @@
 # Figure 3f plotting
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-
 
 def hist(predictions, save_file=''):
   predictions['Highest Del Rate'] = predictions['Highest Del Rate'].apply(lambda x: x * 100)
@@ -58,7 +58,29 @@ def hist(predictions, save_file=''):
     plt.savefig(save_file)
 
 
+# https://stackoverflow.com/questions/43345599/process-pandas-dataframe-into-violinplot
+def box(predictions, save_file=''):
+  data1 = np.random.poisson(lam=1, size=100)
+  data2 = np.random.choice(2, len(data1))
+  data = pd.DataFrame({'Length': data1, 'MH Score': data2})
+  sns.violinplot(x='Length', y='MH Score', data=data, scale='width')
+  plt.ylabel('Counts')
+  #
+  # # Plot GC vs match score, color by length
+  # palette = sns.color_palette('hls', max(data['Length']) + 1)
+  # # for length in range(1, max(data['Length']) + 1):
+  # #   ax = sns.regplot(x='GC', y='MH Score', data=data.loc[data['Length'] == length], color=palette[length - 1], label='Length: %s' % (length))
+  # plt.legend(loc='best')
+  # plt.xlim([0, 1])
+  # plt.title('GC vs. MH Score, colored by MH Length')
+
+  if save_file != '':
+    plt.savefig(save_file)
+
+  plt.close()
+
 if __name__ == '__main__':
-  franz_path = "G:/My Drive/3) Work/Study/TU_Delft_MSc_Nanobiology/3-courses/Machine-Learning-in-Bioinformatics/inDelphi/shared/inDelphi/out/aaa/statistics/prediction_output.csv"
-  predictions = pd.read_csv(franz_path)
-  hist(predictions)
+  # franz_path = "G:/My Drive/3) Work/Study/TU_Delft_MSc_Nanobiology/3-courses/Machine-Learning-in-Bioinformatics/inDelphi/shared/inDelphi/out/aaa/statistics/prediction_output.csv"
+  # predictions = pd.read_csv(franz_path)
+  # hist(predictions)
+  box([], 'C:\\Development\\CS4260\\inDelphi\\out\\aaa\\statistics\\test.pdf')
