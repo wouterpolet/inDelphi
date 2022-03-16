07f42e36a2c5d5f0f3bb2376dea04ae4178afc11
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2022-03-16 09:23:00 +0100

    Update plot_4a.py

--- src-modeling-and-analysis/plot_4a.py
+++ src-modeling-and-analysis/plot_4a.py
@@ -3,16 +3,18 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Source
 # https://stackoverflow.com/questions/68614447/how-to-display-boxplot-in-front-of-violinplot-in-seaborn-seaborn-zorder
+
 # Genotype pearson correlation data
-corr_gentyp_mESC = np.transpose(np.random.uniform(90, 100, 5))
-corr_gentyp_U2OS = np.transpose(np.random.uniform(80, 90, 5))
+corr_gentyp_mESC = np.array([99, 97, 86, 93, 85])
+corr_gentyp_U2OS = np.array([70, 80, 73, 65, 79])
 
+# Preparing dataframe
+corr_df = pd.DataFrame({"mESC": corr_gentyp_mESC, "U2OS": corr_gentyp_U2OS})
+df = corr_df.melt(value_vars=['mESC', 'U2OS'], var_name='group')
 
-# numpy_data = np.array([corr_gentyp_mESC, corr_gentyp_U2OS])
-# df = pd.DataFrame(data=numpy_data, columns=['mESC', 'U2OS'])
-# print(df)
-df = pd.DataFrame(np.random.rand(10, 2)).melt(var_name='group')
+# Plotting violins and boxes
 ax = sns.violinplot(data=df,
                     x='group', y='value',
                     color="#af52f4", inner=None,
@@ -21,4 +23,5 @@ sns.boxplot(data=df,
             x='group', y='value',
             saturation=0.5, width=0.4,
             palette='rocket', boxprops={'zorder': 2}, ax=ax)
+
 plt.show()
